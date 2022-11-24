9cbceecdf77be08cc39686ec9f9296142451c259
davis <zanxiaofeng@163.com>
2022-11-24 20:01:10 +0800

    update

---
+++ web-server-csrf/src/main/java/com/zxf/example/controllers/MoneyTransferController.java
@@ -0,0 +1,26 @@
+package com.zxf.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/money")
+public class MoneyTransferController {
+
+    @GetMapping("/transfer")
+    public ModelAndView transfer_form() {
+        ModelAndView modelAndView = new ModelAndView("money/transfer_form");
+        return modelAndView;
+    }
+
+    @PostMapping("/transfer")
+    public ModelAndView transfer_to(@RequestParam String sourceAccount, String targetAccount, String amount) {
+        System.out.println(amount + " has been transfer from " + sourceAccount + " to " + targetAccount);
+        ModelAndView modelAndView = new ModelAndView("money/transfer_result");
+        modelAndView.addObject("sourceAccount", sourceAccount);
+        modelAndView.addObject("targetAccount", targetAccount);
+        modelAndView.addObject("amount", amount);
+        return modelAndView;
+    }
+}
