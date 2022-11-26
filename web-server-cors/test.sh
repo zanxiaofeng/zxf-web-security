eee80d515d6e95eeec941d4062d7b8a7d44ad3fb
davis <zanxiaofeng@163.com>
2022-11-26 10:37:29 +0800

    update cors

---
+++ web-server-cors/test.sh
@@ -0,0 +1,4 @@
+curl --location --request POST 'http://localhost:8082/cors/post' \
+--header 'Origin: http://abc.zxf.com' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'account=dfdsfsd'
