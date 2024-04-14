652363982c55d354faa10f407f75a2cd91fe4096
Davis ZAN <zanxiaofeng@163.com>
2024-04-14 07:57:36 -0500

    update

--- web-server-security/src/main/java/com/zxf/example/controllers/FileController.java
+++ web-server-security/src/main/java/com/zxf/example/controllers/FileController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
 import java.net.URLConnection;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -61,6 +62,22 @@ public class FileController {
         System.out.println("Folder: " + folderPath.toAbsolutePath());
         Path filePath = folderPath.resolve(fileName).normalize();
         System.out.println("File: " + filePath.toAbsolutePath());
+        //如果Path中有符号链接，并不能解析后比较（解析符号链接需要真实执行文件系统操作）
         return filePath.toAbsolutePath().startsWith(folderPath.toAbsolutePath());
+    }
+
+    public static void main(String[] args) throws IOException {
+        String baseFolder = "var/www/images/";
+        Path myPath = Paths.get(baseFolder).resolve("../../../etc/hosts");
+        //toFile并不执行文件系统操作
+        File myFile = myPath.toFile();
+
+        System.out.println("Path::toString, " + myPath);
+        System.out.println("Path::startsWith, " + myPath.startsWith(baseFolder));
+        System.out.println("Path::toString, " + myPath.normalize());
+        System.out.println("Path::toString, " + myPath.toAbsolutePath());
+        System.out.println("File::exists, " + myFile.exists());
+        System.out.println("File::getAbsolutePath, " + myFile.getAbsolutePath());
+        System.out.println("File::getCanonicalPath, " + myFile.getCanonicalPath());
     }
 }
